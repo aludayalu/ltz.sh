c638417c0c6bdede1585543f6dc71d44a04eacc7
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2026-05-13 11:00:16 +0530

    minor fix

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask, request, send_from_directory, send_file
 
 app = Flask(__name__)
 
-@app.get("/", defaults={"any_route": ""})
+@app.get("/", defaults={"any_route": "index.html"})
 @app.get("/<path:any_route>")
 def catch_all(any_route):
     is_curl = request.headers.get("User-Agent", "").startswith("curl/")
 
     if is_curl:
-        if any_route == "":
+        if any_route == "index.html":
             return send_file("ltzInstallOrUpdate.sh")
         else:
             return send_from_directory("out", any_route)
